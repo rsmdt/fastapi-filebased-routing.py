ef969048c9af4e3f02c3b8ba2b27ea81b1ca33e5
Rudolf Schmidt <me@rudionrails.com>
2026-02-09 00:33:43 +0100

    chore: Bump version to 1.0.1

--- tests/unit/test_public_api.py
+++ tests/unit/test_public_api.py
@@ -113,11 +113,11 @@ def test_all_contains_existing_exports():
     assert "RouteValidationError" in fastapi_filebased_routing.__all__
 
 
-def test_version_is_1_0_0():
-    """__version__ is bumped to 1.0.0."""
+def test_version_is_1_0_1():
+    """__version__ is bumped to 1.0.1."""
     import fastapi_filebased_routing
 
-    assert fastapi_filebased_routing.__version__ == "1.0.0"
+    assert fastapi_filebased_routing.__version__ == "1.0.1"
 
 
 def test_route_import_path():
